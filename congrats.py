117b50ec4f18f62344bfaf0714b649c67c5d65a4
meenu27 <meenusatiya2016@u.northwestern.edu>
2017-03-09 20:48:55 -0600

    Update congrats.py

--- congrats.py
+++ congrats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-WINTER_2017 = ['K. Student', 'J. Student', 'L. Student', 'M. Student', 'Z. Student']
+WINTER_2017 = ['K. Student', 'J. Student', 'L. Student', 'M. Student', 'Z. Student', 'Meenu']
 # ADD A NEW LINE HERE TO ADD YOURSELF TO THE LIST using +=
 
 print 'YAY!'
